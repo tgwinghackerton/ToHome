58d8780eae410fbb194b3c95aec7870541456b07
young <siosio34@nate.com>
2017-08-11 01:57:43 +0900

    Add registerActivity1 file

---
+++ app/src/main/java/b05studio/com/mpf/RegisterFirstActivity.java
@@ -0,0 +1,16 @@
+package b05studio.com.mpf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterFirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_first);
+    }
+
+
+
+}
